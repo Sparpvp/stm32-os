56136c658ab0483c6193ddeba9f613936ef4a03a
Sparpvp <74677348+Sparpvp@users.noreply.github.com>
2024-10-03 18:58:25 +0200

    moved panic handler

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,9 @@
 #![no_std]
 #![no_main]
 
-use core::arch::asm;
+pub mod panic;
+
 use cortex_m_semihosting::hprintln;
-
-#[no_mangle]
-extern "C" fn eh_personality() {}
-#[panic_handler]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-    hprintln!("Aborting: ").unwrap();
-
-    match info.location() {
-        Some(l) => {
-            hprintln!("line {}, file {}", l.line(), l.file()).unwrap();
-        }
-        None => {
-            hprintln!("unable to retrieve panic info").unwrap();
-            unreachable!()
-        }
-    }
-
-    // Call the abort handler
-    abort();
-}
-
-#[no_mangle]
-extern "C" fn abort() -> ! {
-    loop {
-        unsafe { asm!("wfi") }
-    }
-}
 
 #[no_mangle]
 extern "C" fn kmain() -> ! {
---
+++ src/panic.rs
@@ -0,0 +1,29 @@
+use core::arch::asm;
+use cortex_m_semihosting::hprintln;
+
+#[no_mangle]
+extern "C" fn eh_personality() {}
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    hprintln!("Aborting: ").unwrap();
+
+    match info.location() {
+        Some(l) => {
+            hprintln!("line {}, file {}", l.line(), l.file()).unwrap();
+        }
+        None => {
+            hprintln!("unable to retrieve panic info").unwrap();
+            unreachable!()
+        }
+    }
+
+    // Call the abort handler
+    abort();
+}
+
+#[no_mangle]
+extern "C" fn abort() -> ! {
+    loop {
+        unsafe { asm!("wfi") }
+    }
+}
