478a479c72a629775c57f86c1cda3ddb7e9c2cac
Sparpvp <74677348+Sparpvp@users.noreply.github.com>
2024-10-11 20:26:20 +0200

    [early] asm trap handler wrapper (alternative to naked func); various bug fixes

--- src/trap.rs
+++ src/trap.rs
@@ -3,25 +3,26 @@ use core::{arch::asm, ptr::read_volatile};
 const ICSR_ADDR: u32 = 0xE000ED04;
 
 #[no_mangle]
-extern "C" fn rust_trap_handler() {
+extern "C" fn rust_trap_handler(stack_ptr: *const u32) {
+    /*
+        Current bugs:
+            - stack_ptr gets overwritten by PUSH as soon as I do it
+            - for some reason if I call this with blx it doesn't
+                get processed at all
+                => How do I return from here?
+    */
+
     // Save the callee-saved registers onto the stack
     // According to the arm calling convention, those are r4-r7
     unsafe {
         asm!("PUSH {{r4-r7}}");
     };
 
-    let mut return_pc: u16;
+    let mut return_pc: u32;
     unsafe {
-        // sp+24 should point to the PC
-        asm!(
-            "
-            add sp, 24    
-            mov r4, sp
-            sub sp, 24
-        "
-        );
-        asm!("LDR {}, [r4]", out(reg) return_pc);
-    };
+        let pc_ptr = stack_ptr.byte_add(0x18 as usize);
+        asm!("LDR {0}, [{1}]", out(reg) return_pc, in(reg) pc_ptr);
+    }
 
     // Match on the Interrupt Control and State Register (ICSR)
     let icsr: u32 = unsafe { read_volatile(ICSR_ADDR as *const u32) };
@@ -35,6 +36,7 @@ extern "C" fn rust_trap_handler() {
         3 => {
             // Hard Fault
             // panic!("Hard Fault exception triggered!\n");
+            return_pc += 4;
         }
         11 => {
             // SVCall
@@ -53,11 +55,13 @@ extern "C" fn rust_trap_handler() {
         }
     }
 
-    // Get back to next instruction and restore the registers pushed onto the stack
+    // Restore the registers pushed onto the stack
     unsafe {
         asm!(
-            "POP {{r4-r7}}",
-            "bx {}",
+            "
+            mov r2, {0}
+            POP {{r4-r7}}
+        ", 
             in(reg) return_pc
         );
     };
