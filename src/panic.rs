04f99cba58f2f552a43eecd9a5060a0e4310618a
Sparpvp <74677348+Sparpvp@users.noreply.github.com>
2024-11-14 16:27:56 +0100

    removed unneeded dependencies

--- src/panic.rs
+++ src/panic.rs
@@ -1,18 +1,17 @@
 use core::arch::asm;
-use cortex_m_semihosting::hprintln;
 
 #[no_mangle]
 extern "C" fn eh_personality() {}
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-    hprintln!("Panicking...: ").unwrap();
+    // TODO: USART print
 
     match info.location() {
         Some(l) => {
-            hprintln!("line {}, file {}", l.line(), l.file()).unwrap();
+            // TODO USART print
         }
         None => {
-            hprintln!("unable to retrieve panic info").unwrap();
+            // TODO USART print
             unreachable!()
         }
     }
